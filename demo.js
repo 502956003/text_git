ad0638e0035bada8498e695090d87e88092c06b5
yh <502956003@qq.com>
2020-05-09 13:00:40 +0800

    0509 13:00 yh

--- demo.js
+++ demo.js
@@ -1,9 +1,10 @@
 console.log('hahahah')
 console.log('bbbbb')
-console.log('ccc')
+console.log('cccccccc')
 console.log('checkout')
 console.log('branch')
 var a = 10;
 var c = 30;
 var d = 400;
 console.log(a + c)
+console.log(a + d)
